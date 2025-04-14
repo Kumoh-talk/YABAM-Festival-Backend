aee8fcadc371cb48a0de5b294def01930e07fcd6
Kkang <aorl2313@naver.com>
2025-04-14 19:20:12 +0900

    feat: storeEntity 엔티티 설계

---
+++ infra/rdb/pos/src/main/java/com/pos/store/entity/StoreEntity.java
@@ -0,0 +1,81 @@
+package com.pos.store.entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.pos.global.base.entity.BaseEntity;
+import com.pos.store.vo.StorePoint;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "stores")
+@NoArgsConstructor
+@Getter
+@SQLDelete(sql = "UPDATE stores SET deleted_at = NOW() where id=?")
+@SQLRestriction(value = "deleted_at is NULL")
+public class StoreEntity extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "owner_id", nullable = false)
+	private Long ownerId;
+
+	@Column(name = "is_open", nullable = false)
+	private boolean isOpen;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Embedded
+	private StorePoint location;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "head_image_url")
+	private String headImageUrl;
+
+	@Column(name = "university", nullable = false)
+	private String university;
+
+	private StoreEntity(Long ownerId, boolean isOpen, String name, StorePoint location, String description,
+		String headImageUrl, String university) {
+		this.ownerId = ownerId;
+		this.isOpen = isOpen;
+		this.name = name;
+		this.location = location;
+		this.description = description;
+		this.headImageUrl = headImageUrl;
+		this.university = university;
+	}
+
+	public static StoreEntity of(
+		Long ownerId,
+		boolean isOpen,
+		String name,
+		Double latitude,
+		Double longitude,
+		String description,
+		String headImageUrl,
+		String university) {
+		return new StoreEntity(
+			ownerId,
+			isOpen,
+			name,
+			StorePoint.of(latitude, longitude),
+			description,
+			headImageUrl,
+			university
+		);
+	}
+}
