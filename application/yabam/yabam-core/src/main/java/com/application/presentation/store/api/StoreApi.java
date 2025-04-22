3fbf450f7c9923eed361cfec5c562080466fc94d
Kkang <aorl2313@naver.com>
2025-04-22 21:19:29 +0900

    feat: storeController 구현 및 dto 구현

---
+++ application/yabam/yabam-core/src/main/java/com/application/presentation/store/api/StoreApi.java
@@ -0,0 +1,98 @@
+package com.application.presentation.store.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.application.global.config.swagger.ApiErrorResponseExplanation;
+import com.application.global.config.swagger.ApiResponseExplanations;
+import com.application.global.config.swagger.ApiSuccessResponseExplanation;
+import com.application.presentation.store.dto.request.StoreWriteRequest;
+import com.application.presentation.store.dto.response.StoreIdResponse;
+import com.application.presentation.store.dto.response.StoreInfoResponse;
+import com.exception.ErrorCode;
+import com.response.ResponseBody;
+
+import domain.pos.member.entity.UserPassport;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
+
+@Schema(description = "가게 API")
+public interface StoreApi {
+
+	@Operation(
+		summary = "가게 생성 API",
+		description = "가게를 생성합니다."
+	)
+	@ApiResponse(content = @Content(
+		mediaType = "application/json",
+		schema = @Schema(implementation = StoreIdResponse.class)))
+	@ApiResponseExplanations(
+		success = @ApiSuccessResponseExplanation(
+			responseClass = StoreIdResponse.class,
+			description = "가게 생성 성공"
+		)
+	)
+	ResponseEntity<ResponseBody<StoreIdResponse>> createStore(
+		@Parameter(hidden = true) UserPassport userPassport,
+		@RequestBody @Valid final StoreWriteRequest storeWriteRequest);
+
+	@Operation(
+		summary = "가게 조회 API",
+		description = "가게를 조회합니다."
+	)
+	@ApiResponse(content = @Content(
+		mediaType = "application/json",
+		schema = @Schema(implementation = StoreInfoResponse.class)))
+	@ApiResponseExplanations(
+		success = @ApiSuccessResponseExplanation(
+			responseClass = StoreInfoResponse.class,
+			description = "가게 조회 성공"
+		)
+	)
+	ResponseEntity<ResponseBody<StoreInfoResponse>> getStoreInfo(
+		@Schema(description = "가게 고유 ID", example = "1") @RequestParam final Long storeId);
+
+	@Operation(
+		summary = "가게 수정 API",
+		description = "가게를 수정합니다."
+	)
+	@ApiResponse(content = @Content(
+		mediaType = "application/json",
+		schema = @Schema(implementation = StoreIdResponse.class)))
+	@ApiResponseExplanations(
+		success = @ApiSuccessResponseExplanation(
+			responseClass = StoreIdResponse.class,
+			description = "가게 수정 성공"
+		),
+		errors = {
+			@ApiErrorResponseExplanation(errorCode = ErrorCode.NOT_FOUND_STORE),
+			@ApiErrorResponseExplanation(errorCode = ErrorCode.NOT_EQUAL_STORE_OWNER)
+		}
+	)
+	ResponseEntity<ResponseBody<StoreIdResponse>> updateStoreInfo(
+		@Parameter(hidden = true) UserPassport userPassport,
+		@Schema(description = "가게 고유 ID", example = "1") @RequestParam final Long storeId,
+		@RequestBody @Valid final StoreWriteRequest storeWriteRequest);
+
+	@Operation(
+		summary = "가게 삭제 API",
+		description = "가게를 삭제합니다."
+	)
+	@ApiResponseExplanations(
+		success = @ApiSuccessResponseExplanation(
+			description = "가게 삭제 성공"
+		),
+		errors = {
+			@ApiErrorResponseExplanation(errorCode = ErrorCode.NOT_FOUND_STORE),
+			@ApiErrorResponseExplanation(errorCode = ErrorCode.NOT_EQUAL_STORE_OWNER)
+		}
+	)
+	ResponseEntity<ResponseBody<Void>> deleteStore(
+		@Parameter(hidden = true) UserPassport userPassport,
+		@RequestParam final Long storeId);
+}
---
+++ application/yabam/yabam-core/src/main/java/com/application/presentation/store/dto/response/StoreInfoResponse.java
@@ -0,0 +1,45 @@
+package com.application.presentation.store.dto.response;
+
+import domain.pos.store.entity.Store;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Schema(description = "가게 정보 응답 DTO")
+@Builder
+public record StoreInfoResponse(
+	@Schema(description = "가게 ID", example = "1")
+	Long storeId,
+	@Schema(description = "가게 오픈 여부", example = "true")
+	boolean isOpen,
+	@Schema(description = "점주 고유 ID", example = "1")
+	Long userId,
+	@Schema(description = "점주 닉네임", example = "강민기")
+	String userNickname,
+	@Schema(description = "가게 이름", example = "가게 이름")
+	String storeName,
+	@Schema(description = "가게 위도", example = "37.123456")
+	Double latitude,
+	@Schema(description = "가게 경도", example = "127.123456")
+	Double longitude,
+	@Schema(description = "가게 설명", example = "가게 설명")
+	String description,
+	@Schema(description = "가게 대표 이미지 URL", example = "https://example.com/image.jpg")
+	String headImageUrl,
+	@Schema(description = "가게 소속 대학교", example = "서울대학교")
+	String university
+) {
+	public static StoreInfoResponse of(Store store) {
+		return StoreInfoResponse.builder()
+			.storeId(store.getStoreId())
+			.isOpen(store.getIsOpen())
+			.userId(store.getOwnerPassport().getUserId())
+			.userNickname(store.getOwnerPassport().getUserNickname())
+			.storeName(store.getStoreInfo().getStoreName())
+			.latitude(store.getStoreInfo().getLocation().getX())
+			.longitude(store.getStoreInfo().getLocation().getY())
+			.description(store.getStoreInfo().getDescription())
+			.headImageUrl(store.getStoreInfo().getHeadImageUrl())
+			.university(store.getStoreInfo().getUniversity())
+			.build();
+	}
+}
