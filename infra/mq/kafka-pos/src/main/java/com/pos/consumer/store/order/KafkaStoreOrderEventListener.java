c61f6431983c81ca0d49b8afc3886db69e67064f
Kkang <aorl2313@naver.com>
2025-04-27 22:34:09 +0900

    test: kafka 계층 테스트 코드 구현

--- infra/mq/kafka-pos/src/main/java/com/pos/consumer/store/order/KafkaStoreOrderEventListener.java
+++ infra/mq/kafka-pos/src/main/java/com/pos/consumer/store/order/KafkaStoreOrderEventListener.java
@@ -1,6 +1,5 @@
 package com.pos.consumer.store.order;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
@@ -12,25 +11,31 @@ import com.pos.consumer.StoreOrderHandler;
 import com.pos.event.StoreOrderEvent;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
-@ConditionalOnProperty(name = "event.listener", havingValue = "kafka", matchIfMissing = true)
 public class KafkaStoreOrderEventListener {
 	private final StoreOrderHandler storeOrderHandler;
 	private final ObjectMapper mapper;
 
-	@KafkaListener(topics = "store-orders", groupId = "store-order-consumer")
+	@KafkaListener(topics = "${kafka.store.order.topic}", groupId = "${kafka.store.order.group-id}")
 	public void listen(@Header(KafkaHeaders.RECEIVED_KEY) String key, String json) {
+		log.info("KafkaStoreOrderEventListener listen() - key: {}, json: {}", key, json);
 		try {
 			StoreOrderEvent storeOrderEvent = mapper.readValue(json, StoreOrderEvent.class);
 			onOrderEvent(key, storeOrderEvent);
 		} catch (JsonProcessingException e) {
+			log.error("KafkaStoreOrderEventListener listen() - JSON 변환 실패, key: {}, json: {}, error: {}",
+				key,
+				json,
+				e.getMessage());
 			throw new RuntimeException(e);
 		}
 	}
 
-	public void onOrderEvent(String key, StoreOrderEvent storeOrderEvent) {
+	private void onOrderEvent(String key, StoreOrderEvent storeOrderEvent) {
 		storeOrderHandler.handleStoreOrder(key, storeOrderEvent);
 	}
 }
