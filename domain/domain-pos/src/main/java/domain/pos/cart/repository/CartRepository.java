84578a2b459713a7e46ca56034358d4e285ab48b
Kkang <aorl2313@naver.com>
2025-05-03 00:03:36 +0900

    fix: 장바구니 도메인 코드 수정

--- domain/domain-pos/src/main/java/domain/pos/cart/repository/CartRepository.java
+++ domain/domain-pos/src/main/java/domain/pos/cart/repository/CartRepository.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 import domain.pos.cart.entity.Cart;
-import domain.pos.menu.entity.MenuInfo;
 
 @Repository
 public interface CartRepository {
-	Cart postCart(Long receiptId, MenuInfo menuInfo, Integer quantity);
+	// 해당 영수증에 장바구니가 없다면, 장바구니를 생성한다.
+	// 장바구니가 있다면, 장바구니를 quantity 만큼 증가한다.
+	void upsertCart(Long receiptId, Long menuId, Integer quantity);
 
 	void deleteCartMenu(Long receiptId, Long menuId);
 
