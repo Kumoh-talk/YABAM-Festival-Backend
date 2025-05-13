d1e789738b12e8af75850d17f91948582c71d4e6
Kkang <aorl2313@naver.com>
2025-05-14 03:59:10 +0900

    fix: store 조회시 상세 이미지 없을 경우 조회 안되는 에러 해결

--- infra/rdb/pos/src/main/java/com/pos/store/repository/StoreRepositoryImpl.java
+++ infra/rdb/pos/src/main/java/com/pos/store/repository/StoreRepositoryImpl.java
@@ -45,7 +45,7 @@ public class StoreRepositoryImpl implements StoreRepository {
 		StoreEntity storeEntities = queryFactory
 			.select(qStoreEntity)
 			.from(qStoreEntity)
-			.join(qStoreEntity.storeDetailImageEntity, qStoreDetailImageEntity).fetchJoin()
+			.leftJoin(qStoreEntity.storeDetailImageEntity, qStoreDetailImageEntity).fetchJoin()
 			.where(qStoreEntity.id.eq(storeId))
 			.fetchFirst();
 
@@ -128,7 +128,7 @@ public class StoreRepositoryImpl implements StoreRepository {
 	public List<Store> findMyStores(Long userId) {
 		List<StoreEntity> storeEntities = queryFactory.select(qStoreEntity)
 			.from(qStoreEntity)
-			.join(qStoreEntity.storeDetailImageEntity, qStoreDetailImageEntity).fetchJoin()
+			.leftJoin(qStoreEntity.storeDetailImageEntity, qStoreDetailImageEntity).fetchJoin()
 			.where(qStoreEntity.ownerId.eq(userId))
 			.fetch();
 		return storeEntities.stream()
