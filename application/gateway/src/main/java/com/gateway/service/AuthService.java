6d22069bdd5fe29cb1f7ee894cdce7698f5f2f28
siwan9 <si.wan208@gmail.com>
2025-05-14 15:34:16 +0900

    refactor: 로그아웃 validation 추가

--- application/gateway/src/main/java/com/gateway/service/AuthService.java
+++ application/gateway/src/main/java/com/gateway/service/AuthService.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Service;
 
 import com.exception.ErrorCode;
 import com.exception.ServiceException;
+import com.gateway.exception.JwtAccessDeniedException;
+import com.gateway.exception.JwtTokenExpiredException;
+import com.gateway.exception.JwtTokenInvalidException;
+import com.gateway.jwt.JwtAuthentication;
 import com.gateway.jwt.JwtAuthenticationToken;
 import com.gateway.jwt.JwtHandler;
 import com.gateway.jwt.JwtUserClaim;
 import com.vo.Token;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -20,7 +25,23 @@ public class AuthService {
 
 	public Mono<Void> logout(Authentication authentication) {
 		JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken)authentication;
-		return Mono.fromCallable(() -> jwtHandler.parseToken(jwtAuthenticationToken.token()))
+
+		return Mono.fromCallable(() -> {
+				try {
+					if (jwtAuthenticationToken.token() == null) {
+						throw new JwtTokenInvalidException();
+					}
+
+					JwtUserClaim claims = jwtHandler.parseToken(jwtAuthenticationToken.token());
+					return new JwtAuthentication(claims);
+				} catch (ExpiredJwtException e) {
+					throw new JwtTokenExpiredException(e);
+				} catch (JwtAccessDeniedException e) {
+					throw e; // 예외를 그대로 던짐
+				} catch (Exception e) {
+					throw new JwtTokenInvalidException(e);
+				}
+			})
 			.flatMap(claims -> jwtHandler.deleteRefreshToken(String.valueOf(claims.userId())))
 			.then();
 	}
