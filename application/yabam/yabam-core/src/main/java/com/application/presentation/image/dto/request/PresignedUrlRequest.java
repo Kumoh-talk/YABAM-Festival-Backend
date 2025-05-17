6294b51667218f146758167a89c05f082b543291
Kkang <aorl2313@naver.com>
2025-05-17 10:08:18 +0900

    refactor: 코드래빗 리뷰 반영

--- application/yabam/yabam-core/src/main/java/com/application/presentation/image/dto/request/PresignedUrlRequest.java
+++ application/yabam/yabam-core/src/main/java/com/application/presentation/image/dto/request/PresignedUrlRequest.java
@@ -7,14 +7,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
-@Schema(name = "StorePresignedUrlRequest", description = "스토어 프리사인드 URL 요청")
+@Schema(name = "PresignedUrlRequest", description = "스토어 프리사인드 URL 요청")
 public record PresignedUrlRequest(
 	@Schema(description = "스토어 ID", example = "1")
 	@NotNull
 	@Min(value = 1, message = "스토어 ID는 1 이상이어야 합니다.")
 	Long storeId,
 	@Schema(description = "스토어 이미지 속성", example = "STORE_HEAD")
-	@ValidEnum(message = "스토어 이미지 속성은 STORE_HEAD, STORE_DETAIL 중 하나여야 합니다.", enumClass = ImageProperty.class)
+	@ValidEnum(message = "스토어 이미지 속성은 STORE_HEAD, STORE_DETAIL, MENU_IMAGE 중 하나여야 합니다.",
+		enumClass = ImageProperty.class)
 	ImageProperty imageProperty
 ) {
 }
