7793627537606a939699307d1fac02a0d21154e3
siwan9 <si.wan208@gmail.com>
2025-05-18 22:34:56 +0900

    fix : 주문 정보 응답에 주문 시간 포함

--- application/yabam/yabam-core/src/main/java/com/application/presentation/order/dto/response/OrderAndMenusResponse.java
+++ application/yabam/yabam-core/src/main/java/com/application/presentation/order/dto/response/OrderAndMenusResponse.java
@@ -1,6 +1,9 @@
 package com.application.presentation.order.dto.response;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import domain.pos.order.entity.Order;
 import domain.pos.order.entity.vo.OrderStatus;
@@ -14,6 +17,9 @@ public record OrderAndMenusResponse(
 	OrderStatus orderStatus,
 	@Schema(description = "주문 총 가격", example = "20000")
 	Integer totalPrice,
+	@Schema(description = "주문 시간", example = "2023-10-01T10:00:00")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+	LocalDateTime createdAt,
 	@Schema(description = "주문 메뉴 리스트")
 	List<OrderMenuResponse> orderMenus
 ) {
@@ -22,6 +28,7 @@ public record OrderAndMenusResponse(
 			order.getOrderId(),
 			order.getOrderStatus(),
 			order.getTotalPrice(),
+			order.getCreatedAt(),
 			order.getOrderMenus().stream()
 				.map(OrderMenuResponse::from)
 				.toList()
--- application/yabam/yabam-core/src/main/java/com/application/presentation/order/dto/response/OrderInfoResponse.java
+++ application/yabam/yabam-core/src/main/java/com/application/presentation/order/dto/response/OrderInfoResponse.java
@@ -1,5 +1,9 @@
 package com.application.presentation.order.dto.response;
 
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import domain.pos.order.entity.Order;
 import domain.pos.order.entity.vo.OrderStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,13 +15,18 @@ public record OrderInfoResponse(
 	@Schema(description = "주문 상태", example = "RECEIVED")
 	OrderStatus orderStatus,
 	@Schema(description = "주문 총 가격", example = "20000")
-	Integer totalPrice
+	Integer totalPrice,
+
+	@Schema(description = "주문 시간", example = "2023-10-01T10:00:00")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+	LocalDateTime createdAt
 ) {
 	public static OrderInfoResponse from(Order order) {
 		return new OrderInfoResponse(
 			order.getOrderId(),
 			order.getOrderStatus(),
-			order.getTotalPrice()
+			order.getTotalPrice(),
+			order.getCreatedAt()
 		);
 	}
 }
