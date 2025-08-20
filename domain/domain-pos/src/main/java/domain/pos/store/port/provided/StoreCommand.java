39c977c5983f2f98d5267314b1e920edc55c3a1f
Kkang <aorl2313@naver.com>
2025-08-20 16:03:47 +0900

    refactor: @Transactional 어노테이션 구현 클래스에만 작성하는 걸로 수정

--- domain/domain-pos/src/main/java/domain/pos/store/port/provided/StoreCommand.java
+++ domain/domain-pos/src/main/java/domain/pos/store/port/provided/StoreCommand.java
@@ -1,7 +1,5 @@
 package domain.pos.store.port.provided;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.vo.UserPassport;
 
 import domain.pos.store.entity.Store;
@@ -10,13 +8,11 @@ import domain.pos.store.entity.StoreInfo;
 public interface StoreCommand {
 	Store createStore(final UserPassport ownerPassport, final StoreInfo createRequestStoreInfo);
 
-	@Transactional
 	Store updateStoreInfo(
 		UserPassport ownerPassport,
 		Long queryStoreId,
 		StoreInfo modifyStoreInfo);
 
-	@Transactional
 	void deleteStore(
 		UserPassport ownerPassport,
 		Long queryStoreId
