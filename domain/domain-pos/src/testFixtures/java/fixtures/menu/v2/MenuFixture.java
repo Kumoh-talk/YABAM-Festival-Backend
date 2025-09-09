5b1ab796aa08c0299b0bd2b230ab0d2bf9abc859
siwan9 <si.wan208@gmail.com>
2025-09-09 15:32:28 +0900

    fix : test 에러 수정

--- domain/domain-pos/src/testFixtures/java/fixtures/menu/v2/MenuFixture.java
+++ domain/domain-pos/src/testFixtures/java/fixtures/menu/v2/MenuFixture.java
@@ -3,11 +3,14 @@ package fixtures.menu.v2;
 import static fixtures.menu.v2.ValidMenuState.*;
 import static fixtures.menu.v2.state.MenuInfoStateFixture.*;
 
+import java.time.LocalDateTime;
+
 import domain.pos.menu.entity.v2.Menu;
 
 public class MenuFixture {
 
 	public static Menu VALID_MENU() {
-		return Menu.create(VALID_STATE(), VALID_ORDER_1, VALID_STORE_ID_1, VALID_MENU_CATEGORY_ID_1);
+		return Menu.fromInfra(VALID_MENU_ID_1, VALID_STATE(), VALID_ORDER_1, false, false, LocalDateTime.now(),
+			VALID_STORE_ID_1, VALID_MENU_CATEGORY_ID_1);
 	}
 }
