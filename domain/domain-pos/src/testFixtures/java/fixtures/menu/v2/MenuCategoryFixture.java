ce67504e861e8ec6a1a8b6335e22c213fcc75a63
siwan9 <si.wan208@gmail.com>
2025-10-04 20:45:58 +0900

    refactor : 메뉴, 카테고리 관련 접근자 수정

--- domain/domain-pos/src/testFixtures/java/fixtures/menu/v2/MenuCategoryFixture.java
+++ domain/domain-pos/src/testFixtures/java/fixtures/menu/v2/MenuCategoryFixture.java
@@ -4,11 +4,6 @@ import domain.pos.menu.entity.v2.domain.MenuCategory;
 
 public class MenuCategoryFixture {
 	public static MenuCategory VALID_CATEGORY() {
-		return MenuCategory.builder()
-			.id(1L)
-			.name("음료")
-			.order(1)
-			.storeId(1L)
-			.build();
+		return MenuCategory.fromInfra(1L, "음료", 1, 1L);
 	}
 }
